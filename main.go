8d8ddba54b177a125fe13dcc7552861cf3fa6732
Tam Phan <tphan25@vt.edu>
2019-10-18 17:13:54 -0400

    First commit. Created CourseQuery object which contains several fields which are passed as form data to the actual API. Created several setters for this object which return a form of the object with these properties applied, for chaining expressions. The method SendQuery sends a CourseQuery object to the given URL, and returns the response and error given from the server or request itself. Note that CoreCode is a required field, which I set as default to AR% as that is what I saw when using the actual timetable with all CoreCodes selected.

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	//Run the below code to print out the entire HTML document at the timetable URL :')
+	// cq := CreateEmptyQuery().SetCampus("0").SetCrn("82821").SetTermYear("201909")
+	// resp, err := SendQuery(timetableURL, cq)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(string(body))
+	// fmt.Println(cq)
+}
---
+++ query.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"strings"
+)
+
+/*timetableURL constant is where we send our request*/
+const timetableURL = "https://banweb.banner.vt.edu/ssb/prod/HZSKVTSC.P_ProcRequest"
+
+/*CourseQuery is the query object sent directly to the VT Timetable server */
+type CourseQuery struct {
+	Campus       string `json:"CAMPUS"`
+	TermYear     string `json:"TERMYEAR"`
+	CoreCode     string `json:"CORE_CODE"`
+	SubjectCode  string `json:"subj_code"`
+	ScheduleType string `json:"SCHDTYPE"`
+	CourseNumber string `json:"CRSE_NUMBER"`
+	Crn          string `json:"crn"`
+	OpenOnly     string `json:"open_only"`
+}
+
+/*SendQuery sends the query to the given URL.*/
+func SendQuery(inputURL string, cq CourseQuery) (*http.Response, error) {
+	//Required for request to process correctly
+	if len(cq.CoreCode) == 0 {
+		cq.CoreCode = "AR%"
+	}
+
+	req, err := http.NewRequest("POST", inputURL, nil)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		return nil, err
+	}
+
+	q := req.URL.Query()
+	v := reflect.ValueOf(cq)
+	var jsonTag string
+	var tagOnly string
+	var value string
+	for i := 0; i < v.NumField(); i++ {
+		jsonTag = string(reflect.TypeOf(cq).Field(i).Tag)         //Gets struct tag using reflection
+		tagOnly = strings.Trim(string([]rune(jsonTag)[5:]), "\"") //Cuts off first 5 chars
+		value = v.Field(i).Interface().(string)
+		fmt.Println(tagOnly, value)
+		if len(value) > 0 {
+			q.Add(tagOnly, value)
+		}
+	}
+	req.URL.RawQuery = q.Encode()
+	fmt.Println(req.URL.String())
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	return resp, err
+}
+
+/*CreateEmptyQuery creates an empty query object which can be manipulated with setter methods in this file. */
+func CreateEmptyQuery() CourseQuery {
+	cq := CourseQuery{}
+	return cq
+}
+
+/*SetCampus returns the caller CourseQuery object with the campus field set.*/
+func (cq CourseQuery) SetCampus(Campus string) CourseQuery {
+	cq.Campus = Campus
+	return cq
+}
+
+/*SetTermYear returns the caller CourseQuery object with the term year field set.*/
+func (cq CourseQuery) SetTermYear(TermYear string) CourseQuery {
+	cq.TermYear = TermYear
+	return cq
+}
+
+/*SetCoreCode returns the caller CourseQuery object with the term year field set.*/
+func (cq CourseQuery) SetCoreCode(CoreCode string) CourseQuery {
+	cq.CoreCode = CoreCode
+	return cq
+}
+
+/*SetSubjectCode returns the caller CourseQuery object with the term year field set.*/
+func (cq CourseQuery) SetSubjectCode(SubjectCode string) CourseQuery {
+	cq.SubjectCode = SubjectCode
+	return cq
+}
+
+/*SetScheduleType returns the caller CourseQuery object with the term year field set.*/
+func (cq CourseQuery) SetScheduleType(ScheduleType string) CourseQuery {
+	cq.ScheduleType = ScheduleType
+	return cq
+}
+
+/*SetCourseNumber returns the caller CourseQuery object with the term year field set.*/
+func (cq CourseQuery) SetCourseNumber(CourseNumber string) CourseQuery {
+	cq.CourseNumber = CourseNumber
+	return cq
+}
+
+/*SetCrn returns the caller CourseQuery object with the term year field set.*/
+func (cq CourseQuery) SetCrn(Crn string) CourseQuery {
+	cq.Crn = Crn
+	return cq
+}
+
+/*SetOpenOnly returns the caller CourseQuery object with the term year field set.*/
+func (cq CourseQuery) SetOpenOnly(OpenOnly string) CourseQuery {
+	cq.OpenOnly = OpenOnly
+	return cq
+}
